034fb30481a6417f81a7cbf59ca8641882a9c332
David Lidebrandt <david.lidebrandt@gmail.com>
2023-11-17 21:52:47 +0100

    Inital commit

---
+++ src/test/java/com/example/labb3/Labb3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labb3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labb3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

f88c8dce30ceaab9812bdf681743c137eafcffdf
David Lidebrandt <david.lidebrandt@gmail.com>
2023-12-05 02:09:51 +0100

    Add security filter chain

---
+++ src/main/java/com/example/labb3/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.labb3;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http.httpBasic(Customizer.withDefaults()).authorizeHttpRequests(authorize -> {
+                authorize.requestMatchers(HttpMethod.GET, "/api/categories").permitAll().
+                anyRequest().denyAll();
+    }).build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(32);
+    }
+
+}

b3120c9d8feb9f3df12b59ace6702c3d6a6e6472
David Lidebrandt <david.lidebrandt@gmail.com>
2023-12-05 12:26:29 +0100

    Disable cors and csrf

--- src/main/java/com/example/labb3/SecurityConfig.java
+++ src/main/java/com/example/labb3/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 
 @Configuration
@@ -15,7 +16,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return http.httpBasic(Customizer.withDefaults()).authorizeHttpRequests(authorize -> {
+    return http.httpBasic(Customizer.withDefaults()).
+            logout(AbstractHttpConfigurer::disable).
+            csrf(AbstractHttpConfigurer::disable).
+            cors(AbstractHttpConfigurer::disable).
+
+            authorizeHttpRequests(authorize -> {
                 authorize.requestMatchers(HttpMethod.GET, "/api/categories").permitAll().
                 anyRequest().denyAll();
     }).build();
--- src/main/java/com/example/labb3/controllers/TestController.java
+++ src/main/java/com/example/labb3/controllers/TestController.java
@@ -3,8 +3,10 @@ package com.example.labb3.controllers;
 import com.example.labb3.entities.Test;
 import com.example.labb3.mappers.TestMapper;
 import com.example.labb3.services.TestService;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -23,7 +25,10 @@ public class TestController {
     }
 
     @GetMapping("{id}")
-    public int getID(@PathVariable int id) {
+    public int getID(@PathVariable int id, Authentication principal) {
+        System.out.println(
+                principal.getName()
+        );
         return id;
     }
 
